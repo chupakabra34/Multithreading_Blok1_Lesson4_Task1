16f4c9fce402f93b94df31de6c158e1073849f95
Sergei Prekrasnov <sap685@gmail.com>
2022-05-29 21:57:50 +0300

    Multithreading_Blok1_Lesson4_Task1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* Author: Prekrasnov Sergei
+ * \* Date: 25.05.2022
+ * \* ----- group JAVA-27 -----
+ * \*
+ * \* Description: Домашнее задание к занятию 1.4 Коллекции для параллельной (конкурирующей) работы.
+ * \*
+ * \* Задача: 1. Колл-центр
+ * \
+ */
+public class Main {
+    public static final int CALLS = 60;
+    public static final int SPECIALISTS = 10;
+
+    public static void main(String[] args) {
+        CallCenter center = new CallCenter();
+        for (int i = 1; i <= CALLS; i++) {
+            new Thread(null, center::call, "№ " + i).start();
+        }
+
+        for (int i = 1; i <= SPECIALISTS; i++) {
+            new Thread(null, center::takeTheCall, "№ " + i).start();
+        }
+    }
+}
